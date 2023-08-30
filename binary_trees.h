13c32cfc669b9327e621c0a964133020c1d3c0ea
Beckyshee <wanjirubecky.rw@gmail.com>
2023-08-30 11:40:52 -0700

    converts a Binary Max Heap to a sorted array of integers

--- binary_trees.h
+++ binary_trees.h
@@ -70,5 +70,7 @@ size_t tree_size_h(const binary_tree_t *tree);
 void _preorder(heap_t *tree, heap_t **node, size_t height);
 void heapify(heap_t *root);
 int heap_extract(heap_t **root);
+size_t tree_size(const binary_tree_t *tree);
+int *heap_to_sorted_array(heap_t *heap, size_t *size);
 
 #endif
